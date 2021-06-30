046d7ad1c5cd132bbd8dddd3f5ae1ead11982013
DESKTOP-8QREPJ2\Ummay Haani <umayhaani981@gmail.com>
2021-06-30 22:37:15 +0500

    completed

--- src/users/admin/Admin.js
+++ src/users/admin/Admin.js
@@ -1,74 +1,16 @@
-//import { Button } from "../Buttons/Buttons";
-import React, { Component } from "react";
-
-import { MenuItems } from "./MenuItems-admin";
-import { NavLink } from "react-router-dom";
-import logo from "../../assets/PPS5.jpeg";
-import "./admin.css";
-//import {image} from "./images";
-//import { TextField } from "@material-ui/core";
+import React from "react";
+import CourseAdmins from "./CourseAdmins";
+import SuperAdmin from "./SuperAdmin";
+import { useLocalStorage } from "../../LocalStorage/Local";
 
 const Admin = () => {
-    return (
-      <React.Fragment>
+  const [adminType, setAdminType] = useLocalStorage("adminType", "null");
+  return (
+    <>
+      {adminType === "admin" && <SuperAdmin />}
+      {adminType !== "admin" && <CourseAdmins />}
+    </>
+  );
+};
 
-<nav className="NavbarItems-admin">
-        
-<NavLink to="/admin" >
-                  
-        <img className="logo-admin" src={logo} alt="Logo" />
-
-</NavLink>
-        <h1 className="navbar-logo-admin">Admin's Portal</h1>
-
-        <ul className="nav-menu-admin ">
-          {MenuItems.map((item, index) => {
-            return (
-              <div className="nav-links-admin" key={index}>
-                <li>
-                  <NavLink to={item.url} className={item.cName}>
-                    {" "}
-                    {item.title}
-                  </NavLink>
-                  {/* <a className={item.cName} href={item.url}>
-                      {item.title}
-                    </a> */}
-                </li>
-              </div>
-            );
-          })}
-        </ul>
-        <NavLink to="/#" > <button className="button-admin">Log out</button>
-        </NavLink> 
-        
-      </nav>
-
-
-        {/* <nav className="NavbarItems">
-          <img className="logo" src={logo} alt="Logo" />
-          <h1 className="navbar-logo">Admin's Portal</h1> */}
-
-          {/* <div className="menu-icon"></div> */}
-
-          {/* <ul className="nav-menu ">
-            {MenuItems.map((item, index) => {
-              return (
-                <div className="nav-links" key={index}>
-                  <li>
-                    <NavLink to={item.url} className={item.cName}>
-                      {" "}
-                      {item.title}
-                    </NavLink>
-                  </li>
-                </div>
-              );
-            })}
-          </ul>
-          <NavLink to="/addword" > <button className="button-admin">Log out</button></NavLink>
-        </nav> */}
-        <div className="admin-body"> Welcome Admin</div>
-      </React.Fragment>
-    );
-  
-}
 export default Admin;
