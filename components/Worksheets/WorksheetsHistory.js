d0a8ebe76889e2d842591d691dcb5a68f5bf82b1
umayhaani <75578762+umayhaani@users.noreply.github.com>
2021-07-01 00:03:08 +0500

    Delete components directory

--- components/Worksheets/WorksheetsHistory.js
+++
@@ -1,9 +0,0 @@
-import React, { useEffect, useRef, useState } from "react";
-import BasicTable from "../Table/BasicTable";
-import "./CSS/WorksheetsHistory.css";
-
-const WorksheetsHistory = () => {
-  return <BasicTable />;
-};
-
-export default WorksheetsHistory;
